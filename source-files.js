360d16a6ca6350e10cdeb3d5bfa315392326a966
raftario <raftario@users.noreply.github.com>
2021-06-25 22:12:14 +0000

    deploy: d4cbd9ad8502230649f28b3e53421fc3472e8df9

--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["quest_hook"] = {"name":"","files":["inline_hook.rs","lib.rs","libil2cpp.rs"]};
+sourcesIndex["quest_hook"] = {"name":"","files":["backtrace.rs","inline_hook.rs","lib.rs","libil2cpp.rs","tracing_android.rs"]};
 createSourceSidebar();
