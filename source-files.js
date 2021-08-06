9c7284f7ce6f17dafa9db0eb9090cac1fe82c13e
raftario <raftario@users.noreply.github.com>
2021-08-06 09:20:32 +0000

    deploy: 7ac44618abba7c331952110cc1a59578039dfc9b

--- source-files.js
+++ source-files.js
@@ -1,5 +1,5 @@
 var N = null;var sourcesIndex = {};
 sourcesIndex["inline_hook"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["libil2cpp"] = {"name":"","dirs":[{"name":"raw","files":["functions.rs","mod.rs","types.rs"]},{"name":"typecheck","files":["callee.rs","caller.rs","mod.rs","ty.rs"]}],"files":["array.rs","class.rs","exception.rs","field_info.rs","lib.rs","method_info.rs","object.rs","parameter_info.rs","string.rs","ty.rs"]};
-sourcesIndex["quest_hook"] = {"name":"","files":["lib.rs"]};
+sourcesIndex["quest_hook"] = {"name":"","files":["hook.rs","lib.rs","setup.rs"]};
 createSourceSidebar();
