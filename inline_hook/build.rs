14b3ccb68ae4e22efef87205df0e46f5520f5e43
Raphaël Thériault <self@raftar.io>
2021-09-10 14:07:34 -0400

    No wonder the linker can't find it huh

--- inline_hook/build.rs
+++ inline_hook/build.rs
@@ -2,11 +2,11 @@ use cfg_if::cfg_if;
 
 fn main() {
     cfg_if! {
-        if #[cfg(target = "aarch64-linux-android")] {
+        if #[cfg(all(target_arch = "aarch64", target_os = "android"))] {
             cc::Build::new()
                 .file("beatsaber-hook/src/inline-hook/And64InlineHook.cpp")
                 .compile("inline_hook");
-        } else if #[cfg(target = "armv7-linux-androideabi")] {
+        } else if #[cfg(all(target_arch = "arm", target_os = "android"))] {
             cc::Build::new()
                 .file("beatsaber-hook/src/inline-hook/inlineHook.c")
                 .compile("inline_hook");
