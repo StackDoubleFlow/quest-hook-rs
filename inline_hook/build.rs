c4c1c7b1bec51c2630677f6a7e294b8d1a3fa064
Raphaël Thériault <self@raftar.io>
2021-09-10 14:16:01 -0400

    Actually fix inline_hook linking

--- inline_hook/build.rs
+++ inline_hook/build.rs
@@ -1,15 +1,12 @@
-use cfg_if::cfg_if;
-
 fn main() {
-    cfg_if! {
-        if #[cfg(all(target_arch = "aarch64", target_os = "android"))] {
-            cc::Build::new()
-                .file("beatsaber-hook/src/inline-hook/And64InlineHook.cpp")
-                .compile("inline_hook");
-        } else if #[cfg(all(target_arch = "arm", target_os = "android"))] {
-            cc::Build::new()
-                .file("beatsaber-hook/src/inline-hook/inlineHook.c")
-                .compile("inline_hook");
-        }
+    let target = std::env::var("TARGET").unwrap();
+    if target == "aarch64-linux-android" {
+        cc::Build::new()
+            .file("beatsaber-hook/src/inline-hook/And64InlineHook.cpp")
+            .compile("inline_hook");
+    } else if target == "armv7-linux-androideabi" {
+        cc::Build::new()
+            .file("beatsaber-hook/src/inline-hook/inlineHook.c")
+            .compile("inline_hook");
     }
 }
